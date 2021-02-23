9720ba9f30de221975b1b9af5c405497f1cdb00d
anand <verma@anand>
2021-02-23 11:00:57 +0530

    hello pushed

---
+++ src/main/java/com/coforge/demo/App.java
@@ -0,0 +1,13 @@
+package com.coforge.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
